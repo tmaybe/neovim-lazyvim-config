e5add048114726d2a66c37cc1a56e5a01c6bae8d
Tomas Apodaca <tomas@navapbc.com>
2023-01-29 20:44:24 -0800

    add tpope git plugins & configure

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- disable the lazygit keymaps
+vim.keymap.del("n", "<leader>gg")
+vim.keymap.del("n", "<leader>gG")
+
+-- run oi with the word under the cursor, specifying a commit
+vim.keymap.set(
+  "n",
+  "<Leader>yoi",
+  "yiw:!oi<Space>-c<Space><C-r>0<CR>",
+  { silent = true, desc = "Open commit hash under cursor in GitHub" }
+)
