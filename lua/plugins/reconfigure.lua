94e5331bcdfbd733dc4f6cb037fd76d9f153652b
Tomas Apodaca <tomas@navapbc.com>
2023-02-06 14:15:05 -0800

    no animation for indent lines

--- lua/plugins/reconfigure.lua
+++ lua/plugins/reconfigure.lua
@@ -7,6 +7,15 @@ return {
       colorscheme = "tokyonight-night",
     },
   },
+  -- no animation for indent lines
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
   -- disable neo-tree
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- remove neo-tree extension to lualine
