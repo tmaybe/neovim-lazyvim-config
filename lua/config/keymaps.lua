ffd9ee942afd31933e90409f7f6fa54a62aa424a
Tomas Apodaca <tomas@navapbc.com>
2023-02-07 20:13:53 -0800

    let's try lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- disable the lazygit keymaps
-vim.keymap.del("n", "<leader>gg")
-vim.keymap.del("n", "<leader>gG")
 
 -- run oi with the word under the cursor, specifying a commit
 -- https://github.com/tomas-nava/laptop-setup/blob/base/bin/oi
