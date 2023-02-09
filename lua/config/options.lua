8eaa72d5e1dc982e4d1528d7709938acaba80f19
Tomas Apodaca <tomas@navapbc.com>
2023-02-09 10:27:29 -0800

    don't show spaces/tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.list = false -- Don't show invisible characters (tabs, spaces, etc.)
 vim.opt.relativenumber = false -- relative line numbers off by default
