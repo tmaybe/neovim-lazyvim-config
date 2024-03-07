ea6a9a1b3a96ec2e4c4b43b21fdb369ab97561d4
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2024-03-07 08:25:30 -0800

    always show full source on markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- always show full source on markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
