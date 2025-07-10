6b4d9bc5cd6c4c623b6365d6008389726556e81f
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2025-07-10 11:46:44 -0700

    show full source on quarto files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- always show full source on markdown files
+-- always show full source on markdown and quarto files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "markdown" },
+  pattern = { "markdown", "quarto" },
   callback = function()
     vim.wo.conceallevel = 0
   end,
